1ef51045bd9bb5eb732c23ee783b52c65e8db217
sunss <sss@dataqin.com>
2021-07-09 18:39:02 +0800

    📦 add：test css module

--- src/pages/User/Register.tsx
+++ src/pages/User/Register.tsx
@@ -1,10 +1,19 @@
+import './index.less';
+
 import React from 'react';
 
+import clas from './index.module.less';
+
 const Register: React.FC = () => {
+  // 天然支持 ES6
+  const obj = { a: 1, b: 2 };
+  const mergeObj = { ...obj, c: 123 };
   return (
     <div>
       <h2>Register</h2>
       <div>content:</div>
+      <div className="font">fonst size == 50px</div>
+      <div className={clas.red}>{JSON.stringify(mergeObj, null, 4)}</div>
     </div>
   );
 };
