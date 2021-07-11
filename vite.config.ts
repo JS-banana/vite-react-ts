a67aedc9191a9d6f2d3f9f850b1e64d0272d92ee
sunss <sss213018@163.com>
2021-07-11 23:08:44 +0800

    👌 antd按需加载配置优化：lib=>es

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       libList: [
         {
           libName: 'antd',
-          style: (name) => `antd/lib/${name}/style/index.less`,
+          style: (name) => `antd/es/${name}/style/index.less`,
         },
       ],
     }),
