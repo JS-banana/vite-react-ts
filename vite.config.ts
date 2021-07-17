cf78e438fb2ae933a610f5e68af4d762c98e6fce
sunss <sss213018@163.com>
2021-07-17 18:49:30 +0800

    👌 update：vite-plugin-imp => vite-plugin-style-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import legacy from '@vitejs/plugin-legacy';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import fs from 'fs';
 // @ts-ignore
@@ -5,13 +6,18 @@ import fs from 'fs';
 import lessToJS from 'less-vars-to-js';
 import path from 'path';
 import { defineConfig } from 'vite';
-import vitePluginImp from 'vite-plugin-imp';
+// vite-plugin-imp 该插件按需加载存在部分样式丢失的情况
+// import vitePluginImp from 'vite-plugin-imp';
+// 由于 vite 本身已按需导入了组件库，因此仅样式不是按需导入的，因此只需按需导入样式即可。
+import styleImport from 'vite-plugin-style-import';
 
 import config from './config';
 
 const env = process.argv[process.argv.length - 1];
 const base = config[env]?.baseUrl || './';
+console.log('process.argv', process.argv);
 console.log('env：', env);
+console.log('import.meta', import.meta);
 
 const themeVariables = lessToJS(
   fs.readFileSync(path.resolve(__dirname, './config/variables.less'), 'utf8'),
@@ -22,16 +28,35 @@ export default defineConfig({
   plugins: [
     reactRefresh(),
     // 配置按需引入antd
-    vitePluginImp({
-      libList: [
+    // vitePluginImp({
+    //   libList: [
+    //     {
+    //       libName: 'antd',
+    //       style: (name) => `antd/es/${name}/style/index.less`,
+    //     },
+    //   ],
+    // }),
+    styleImport({
+      libs: [
         {
-          libName: 'antd',
-          style: (name) => `antd/es/${name}/style/index.less`,
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
+          },
         },
       ],
     }),
-  ],
+    env === 'legacy' &&
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      }),
+  ].filter(Boolean),
   css: {
+    // modules: {
+    //   localsConvention: 'camelCaseOnly',
+    // },
     preprocessorOptions: {
       less: {
         // 支持内联 JavaScript，支持 less 内联 JS
@@ -42,10 +67,14 @@ export default defineConfig({
     },
   },
   resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './'),
-      '@': path.resolve(__dirname, 'src'),
-    },
+    alias: [
+      { find: /^~/, replacement: path.resolve(__dirname, './') },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+    // alias: {
+    //   '~': path.resolve(__dirname, './'),
+    //   '@': path.resolve(__dirname, 'src'),
+    // },
   },
   server: {
     port: 3001, // 开发环境启动的端口
@@ -54,7 +83,7 @@ export default defineConfig({
         // 当遇到 /api 路径时，将其转换成 target 的值
         target: 'http://127.0.0.1:8080/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // 将 /api 重写为空
+        rewrite: (pre) => pre.replace(/^\/api/, ''), // 将 /api 重写为空
       },
     },
   },
