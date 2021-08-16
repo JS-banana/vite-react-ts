c636538f0ec6c913cbc2a1a33f50ad1bfad4b918
sunss <sss213018@163.com>
2021-08-16 23:33:37 +0800

    style: 删除注释

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,29 +1,9 @@
 module.exports = {
   semi: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   singleQuote: true,
   printWidth: 90,
   tabWidth: 2,
   jsxBracketSameLine: true,
-  endOfLine: "auto",
+  endOfLine: 'auto',
 };
-
-// {
-//   "arrowParens": "always",
-//   "bracketSpacing": true,
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "jsxSingleQuote": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "requirePragma": false,
-//   "semi": true,
-//   "singleQuote": false,
-//   "tabWidth": 2,
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-//   "printWidth": 100
-// }
